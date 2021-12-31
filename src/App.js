e7c32f12a60c207613d0550145a73fdf317257a3
Phil Bohnenkamp <phil.bohnenkamp@gmail.com>
2021-12-30 20:41:13 -0600

    added config and sample code for react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from './logo.svg'
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <Routes>
+            <Route path="/foo" element={
+              <p>
+                I AM FOO! Hear me ROAR!
+              </p>
+            }>
+            </Route>
+            <Route path="/" element={
+              <>
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </>
+            }>
+            </Route>
+          </Routes>
+        </header>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
